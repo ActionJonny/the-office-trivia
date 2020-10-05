797c9d4654b1e44780b5107fe0664b5828ab88a4
Jon Willemain <jon.willemain@gmail.com>
2020-10-05 16:33:08 -0600

    add staging

--- knexfile.js
+++ knexfile.js
@@ -11,6 +11,20 @@ module.exports = {
     },
   },
 
+  staging: {
+    production: {
+      client: 'pg',
+      connection: `${process.env.DATABASE_URL}?ssl=true`,
+      migrations: {
+        directory: './db/migrations'
+      },
+      useNullAsDefault: true,
+      seeds: {
+        directory: './db/seeds/dev'
+      }
+    }
+  }
+
   production: {
     client: 'pg',
     connection: `${process.env.DATABASE_URL}?ssl=true`,
